f0db569d89b35d6e8c4e935287d394e672a983d2
reza ul <rezaul.bd77@gmail.com>
2018-08-02 15:37:53 +0600

    account activaton

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
 config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '.herokuapp.com'
+  host = 'immense-shore-62733.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
